de8762468637cd4bc9d2c6fb4321cf9ddea0e326
shariful8523 <mdsharifulislam8523@gmail.com>
2025-09-08 00:33:23 +0600

    added the responcive funcotional navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import {useLocation} from 'react-router-dom'
+
+
 
 const App = () => {
+
+
+  const isOwnerPath = useLocation().pathname.includes("owner");
   return (
+
     <div>
-      
+
+     { !isOwnerPath &&  <Navbar/>}
+
     </div>
+
   );
 };
 
